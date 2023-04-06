381c155a76bb5c2ffbfe0313d263f836e443ef3d
agungsptr <agung.e.sptr@gmail.com>
2023-04-06 11:10:22 +0700

    Initial Commit

---
+++ MewMath/MewMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MewMathApp.swift
+//  MewMath
+//
+//  Created by Agung Saputra on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MewMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
