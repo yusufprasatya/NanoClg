56becc693838764650a08b0cc7b302b9883ecf2d
Rizky Dwi Hadisaputro <rizky.dwiixg36@gmail.com>
2023-04-14 00:01:57 +0700

    feature/splash-screen

--- MewMath/Views/ContentView.swift
+++ MewMath/Views/ContentView.swift
@@ -1,7 +1,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isActive: Bool = false
+    
     var body: some View {
+        Group {
+            if isActive {
+                Main()
+            } else {
+                Splash()
+                    .navigationBarBackButtonHidden(true)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            withAnimation {
+                                isActive = true
+                            }
+                        }
+                    }
+            }
+        }
+        
+    }
+}
+
+struct Splash: View{
+    var body: some View {
+        ZStack {
+            Color("BgBlue").edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    Image("Cloud")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: 210, maxHeight: 120)
+                        .offset(x: -170)
+                    Image("Cloud")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: 170, maxHeight: 110)
+                        .offset(y: 80)
+                    Image("Cloud")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: 230, maxHeight: 100)
+                        .offset(x: 150, y: -30)
+                }.padding(.top, 40)
+                Spacer()
+            }
+            VStack {
+                Image("Title")
+                    .shadow(color: Color(red: 255 / 255, green: 104 / 255, blue: 103 / 255).opacity(0.5),radius: 10)
+            }
+            VStack {
+                Spacer()
+                Image("Land")
+                    .resizable()
+                    .scaledToFit()
+            }.edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+}
+
+struct Main: View{
+    var body: some View{
         NavigationStack() {
             ZStack {
                 Color("BgBlue").edgesIgnoringSafeArea(.all)
@@ -54,7 +117,6 @@ struct ContentView: View {
                 }
             }
         }
-        
     }
 }
 
