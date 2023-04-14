02ea1b7273ff4d5005f02495fe446a4f5ca77e77
Ferry Dwianta P <anta.ferry40@gmail.com>
2023-04-14 14:36:15 +0700

    feature/successful-message: done

--- MewMath/Views/Question1View.swift
+++ MewMath/Views/Question1View.swift
@@ -165,7 +165,7 @@ struct Question1View: View {
                             .fill(Color("BrokenWhite"))
                             .frame(height: 150)
                             .overlay(VStack {
-                                Text("Maaf dick kamu salah, coba lagi!!!")
+                                Text("Maaf dik kamu salah, coba lagi!!!")
                                     .foregroundColor(Color.black)
                                     .multilineTextAlignment(.center)
                                     .padding(.horizontal, 30)
@@ -175,7 +175,10 @@ struct Question1View: View {
                             .padding(.all, 100)
                     }
                 }
+                
+                SuccessfulMessage()
             }
+            
         }
         .navigationBarBackButtonHidden(true)
     }
@@ -367,5 +370,91 @@ struct OptionsBox: View {
 struct QuestionView_Previews: PreviewProvider {
     static var previews: some View {
         Question1View()
+    }
+}
+
+struct SuccessfulMessage: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .opacity(0.5)
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack(alignment: .topTrailing) {
+                RoundedRectangle(cornerRadius: 24)
+                    .strokeBorder(Color("Pink"), lineWidth: 7)
+                    .background(RoundedRectangle(cornerRadius: 24).fill(Color.white))
+                    .frame(height: 375)
+                    .overlay(
+                        VStack {
+                            Text("Yeay!")
+                                .foregroundColor(Color("Pink"))
+                                .fontWeight(.bold)
+                                .font(.system(size: 24, design: .rounded))
+                                .padding(.top, 10)
+                            Spacer()
+                            HStack(alignment: .bottom) {
+                                Image("BallFillGreen2")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50)
+                                    .offset(y: -5)
+                                Image("Cat")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 110)
+                                Image("BallFill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 25)
+                                    .offset(y: -20)
+                            }
+                            Spacer()
+                            Text("Kamu berhasil menjawab soal cerita tentang pertambahan")
+                                .foregroundColor(Color.black)
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 30)
+                                .font(.system(size: 16))
+                            Spacer()
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Text("PEMBAHASAN")
+                                    .padding(.vertical, 14)
+                                    .padding(.horizontal, 60)
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 12, design: .rounded))
+                                    .fontWeight(.bold)
+                                    .background(
+                                        RoundedRectangle(
+                                            cornerRadius: 12,
+                                            style: .continuous
+                                        )
+                                        .fill(Color("Pink"))
+                                    )
+                            }
+                            
+                            Button {
+                                
+                            } label: {
+                                Text("KEMBALI")
+                                    .foregroundColor(Color("Grey"))
+                                    .font(.system(size: 12, design: .rounded))
+                                    .fontWeight(.bold)
+                            }
+                            Spacer()
+                        }
+                            .padding(.vertical))
+                
+                    .padding(.horizontal, 40)
+                
+                Image("Medali")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80)
+                    .offset(x: -10, y: -40)
+            }
+        }
     }
 }
