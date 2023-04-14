16b24d15beb063fc9de7ad9da04962b33a5e65a3
Rizky Dwi Hadisaputro <rizky.dwiixg36@gmail.com>
2023-04-14 22:59:29 +0700

    feature/background-music

---
+++ MewMath/MusicPlayer.swift
@@ -0,0 +1,31 @@
+//
+//  MusicPlayer.swift
+//  MewMath
+//
+//  Created by Rizky Dwi Hadisaputro on 14/04/23.
+//
+
+import Foundation
+import AVFoundation
+
+class MusicPlayer {
+    static let shared = MusicPlayer()
+    var player: AVAudioPlayer?
+    
+    func startBackgroundMusic() {
+        if let path = Bundle.main.path(forResource: "copycat", ofType: "mp3") {
+            do {
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                player?.numberOfLoops = -1 // Infinite loop
+                player?.play()
+            } catch {
+                print("Could not find the music file.")
+            }
+        }
+    }
+    
+    func stopBackgroundMusic() {
+        player?.stop()
+        player = nil
+    }
+}
